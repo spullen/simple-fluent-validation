5b8527e993141b493c7a9437bc03fe693fa6f82b
Scott Pullen <s.pullen05@gmail.com>
2018-03-16 06:15:00 -0400

    adding comments to the Validator interface

--- src/main/java/net/scottpullen/validation/validators/Validator.java
+++ src/main/java/net/scottpullen/validation/validators/Validator.java
@@ -2,8 +2,31 @@ package net.scottpullen.validation.validators;
 
 import net.scottpullen.validation.ValidationError;
 
+/**
+ * Represents a test for a value or values
+ * 
+ */
 public interface Validator {
+    /**
+     * Tests whether the Validator for the unit under test is valid
+     *
+     * @return boolean isValid
+     */
     boolean isValid();
+
+    /**
+     * Tests whether the Validator for the unit under test is not valid
+     *
+     * This should generally be the inverse of isValid()
+     *
+     * @return boolean isInvalid
+     */
     boolean isInvalid();
+
+    /**
+     * Builds a ValidationError for the validator
+     *
+     * @return ValidationError
+     */
     ValidationError buildValidationError();
 }
