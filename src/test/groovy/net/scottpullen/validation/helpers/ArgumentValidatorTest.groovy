c8cf48b68f6673660cff84b8c5f3330edf820a34
Scott Pullen <s.pullen05@gmail.com>
2018-03-17 15:39:36 -0400

    adding test for 100% test coverage

--- src/test/groovy/net/scottpullen/validation/helpers/ArgumentValidatorTest.groovy
+++ src/test/groovy/net/scottpullen/validation/helpers/ArgumentValidatorTest.groovy
@@ -3,6 +3,15 @@ package net.scottpullen.validation.helpers
 import spock.lang.Specification
 
 class ArgumentValidatorTest extends Specification {
+    // really just to get 100% test coverage...
+    void "constructor"() {
+        when:
+        ArgumentValidation argumentValidation = new ArgumentValidation()
+
+        then:
+        argumentValidation != null
+    }
+
     void "#require"() {
         given:
         String test = null
