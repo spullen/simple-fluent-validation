770f6f12c6600b299ed8d53ef73aca39e5bcc8b3
Scott Pullen <s.pullen05@gmail.com>
2018-03-17 15:21:23 -0400

    fixing typo in name

--- src/test/groovy/net/scottpullen/validation/helpers/ArgumentValidatorTest.groovy
+++ src/test/groovy/net/scottpullen/validation/helpers/ArgumentValidatorTest.groovy
@@ -2,7 +2,7 @@ package net.scottpullen.validation.helpers
 
 import spock.lang.Specification
 
-class ArgumentValidtorTest extends Specification {
+class ArgumentValidatorTest extends Specification {
     void "#require"() {
         given:
         String test = null
